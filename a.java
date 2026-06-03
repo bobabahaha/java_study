c196b19de1e674f4eb45e71ee70e6e2a285acec8
bobaba <1551419517@qq.com>
2026-06-03 21:35:31 +0800

    java 学习测试

--- a.java
+++ a.java
@@ -1,1 +1,15 @@
-eqafqag
+import java.util.Scanner;
+
+public class a{
+    public static void main(String[] args){
+
+
+        Scanner in  = new Scanner(System.in);
+        String out_line = in.nextLine();
+
+        // String ss = "adfasdfa";
+        // String s1 = ss.substring(1,3);
+        System.out.println(out_line);
+
+    }
+}
